1f874755eb330129fa64f11669bb75cd9da1c688
irene908 <70359089+irene908@users.noreply.github.com>
2020-09-01 13:54:22 +0530

    Add files via upload

---
+++ assignment_2.sql
@@ -0,0 +1,17 @@
+create table assign_2 (
+						Name varchar(20),
+                        Joker Int,
+                        Avengers_Endgame Int,
+                        Lion_King Int,
+                        Forrest_Gump Int,
+                        Shutter_Island Int,
+                        Black_Panther Int
+) ;
+
+insert into `assign_2` values ('Rijo', null, 3,3,null,null,4),
+('Blessna',5,5,null,5,5,4),
+('Naveetha',3,5,null,null,null,5),
+('Aarathi',5,4,5,5,5,4),
+('Jaise',null,null,4,null,null,null) ;
+
+select * from `assign_2`; 
